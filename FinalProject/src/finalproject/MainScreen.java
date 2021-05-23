0654cda8fd6c8ab55c45bb618bbbbf3e96420d21
Khadija-Asif <81873642+Khadija-Asif@users.noreply.github.com>
2021-05-24 02:46:58 +0500

    EditCommit

--- FinalProject/src/finalproject/MainScreen.java
+++ FinalProject/src/finalproject/MainScreen.java
@@ -248,7 +248,7 @@ public class MainScreen extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        MainMenu main = new MainMenu();
+        CustomerMenu main = new CustomerMenu();
         main.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
